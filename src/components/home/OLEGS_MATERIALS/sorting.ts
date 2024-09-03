a60349559080ab96e305a3232bdd6fa7b489bb74
birdnero <neroforwork@gmail.com>
2024-09-03 16:35:32 +0300

    #prepearing files for oleg

---
+++ src/components/home/OLEGS_MATERIALS/sorting.ts
@@ -0,0 +1,9 @@
+export type TypePostPrewiew = {
+    url: string,
+    name: string,
+    date: string,
+    rating:number,
+    ppl_rated_it: number,
+}
+
+export type TypeSorting = "date_up" | "date|down" | "rating_up" | "rating_down"
